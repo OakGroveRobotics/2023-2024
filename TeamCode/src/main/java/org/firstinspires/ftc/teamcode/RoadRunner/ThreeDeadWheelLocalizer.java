38a9e4deade472a05d4d8760f2ff64a6a03c3dfa
murra <murray.samuel.t@gmail.com>
2023-10-01 11:20:04 -0500

    Added ThreeDeadWheelLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/ThreeDeadWheelLocalizer.java
@@ -30,9 +30,9 @@ public final class ThreeDeadWheelLocalizer implements Localizer {
     private int lastPar0Pos, lastPar1Pos, lastPerpPos;
 
     public ThreeDeadWheelLocalizer(HardwareMap hardwareMap, double inPerTick) {
-        par0 = new RawEncoder(hardwareMap.get(DcMotorEx.class, "par0"));
-        par1 = new RawEncoder(hardwareMap.get(DcMotorEx.class, "par1"));
-        perp = new RawEncoder(hardwareMap.get(DcMotorEx.class, "perp"));
+        par0 = new RawEncoder(hardwareMap.get(DcMotorEx.class, "left_back_drive"));
+        par1 = new RawEncoder(hardwareMap.get(DcMotorEx.class, "right_front_drive"));
+        perp = new RawEncoder(hardwareMap.get(DcMotorEx.class, "left_front_drive"));
 
         lastPar0Pos = par0.getPositionAndVelocity().position;
         lastPar1Pos = par1.getPositionAndVelocity().position;
