b70000e7f0cd111dc11258f94da426f8e4b60dc6
murra <murray.samuel.t@gmail.com>
2023-11-11 12:51:46 -0600

    I'm not entirely sure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest.java
@@ -5,6 +5,8 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Twist2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.dashboard.FtcDashboard;
 
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrive;
 
@@ -12,6 +14,8 @@ public class LocalizationTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+            FtcDashboard dashboard = FtcDashboard.getInstance();
+            TelemetryPacket packet = new TelemetryPacket();
 
             waitForStart();
 
@@ -25,6 +29,17 @@ public class LocalizationTest extends LinearOpMode {
                 ));
 
                 drive.updatePoseEstimate();
+
+//                packet.fieldOverlay()
+//                        .setStrokeWidth(1)
+//                        .setStroke("goldenrod")
+//                        .strokeCircle(0, 0, ORBITAL_RADIUS)
+//                        .setFill("black")
+//                        .fillPolygon(bxPoints, byPoints);
+//                dashboard.sendTelemetryPacket(packet);
+
+
+
 
                 telemetry.addData("x", drive.pose.position.x);
                 telemetry.addData("y", drive.pose.position.y);
