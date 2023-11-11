42520b7d71f58c6c2fecdf7f3e2e874ffca9dd5b
murra <murray.samuel.t@gmail.com>
2023-11-11 14:19:31 -0600

    This should work 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/tuning/LocalizationTest.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrive;
 public class LocalizationTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(72, -72, 0));
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(62, -62, 0));
             FtcDashboard dashboard = FtcDashboard.getInstance();
             TelemetryPacket packet1 = new TelemetryPacket();
 
@@ -32,7 +32,7 @@ public class LocalizationTest extends LinearOpMode {
 
                 TelemetryPacket packet2 = new TelemetryPacket();
                 packet2.fieldOverlay()
-                    .drawImage("/dash/ftc.jpg", drive.pose.position.x, drive.pose.position.y, 20, 20, Math.toRadians(90), drive.pose.position.x, drive.pose.position.y, false);
+                    .drawImage("/dash/ftc.jpg", drive.pose.position.x, drive.pose.position.y, 20, 20, drive.pose.heading.log(), drive.pose.position.x, drive.pose.position.y, false);
                 dashboard.sendTelemetryPacket(packet2);
 
 
