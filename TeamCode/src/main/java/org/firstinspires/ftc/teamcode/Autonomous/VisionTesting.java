520516a0c78d3c205d493a5753eb36165707098e
murra <murray.samuel.t@gmail.com>
2024-01-27 20:08:26 -0600

    Chnaged itemFinder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/VisionTesting.java
@@ -83,36 +83,18 @@ public class VisionTesting extends OpMode {
         if((itemFinder.getSelected() == ColorProcessorImpl.Selected.LEFT)){
             Actions.runBlocking(
                     drive.actionBuilder(drive.pose)
-                            .splineTo(
-                                    (new Vector2d(
-                                            30,
-                                            30)),
-                                    Math.PI / 2,
-                                    new TranslationalVelConstraint(5))
                             .splineTo(new Vector2d(-6, 0), Math.PI)
                             .build());
         }
         else if ((itemFinder.getSelected() == ColorProcessorImpl.Selected.MIDDLE)) {
             Actions.runBlocking(
                     drive.actionBuilder(drive.pose)
-                            .splineTo(
-                                    (new Vector2d(
-                                            30,
-                                            30)),
-                                    Math.PI / 2,
-                                    new TranslationalVelConstraint(5))
                             .splineTo(new Vector2d(6, 0), Math.PI)
                             .build());
         }
         else if ((itemFinder.getSelected() == ColorProcessorImpl.Selected.RIGHT)) {
             Actions.runBlocking(
                     drive.actionBuilder(drive.pose)
-                            .splineTo(
-                                    (new Vector2d(
-                                            30,
-                                            30)),
-                                    Math.PI / 2,
-                                    new TranslationalVelConstraint(5))
                             .splineTo(new Vector2d(0, 6), Math.PI)
                             .build());
         }
@@ -122,94 +104,5 @@ public class VisionTesting extends OpMode {
         telemetry.addData("Identified", itemFinder.getSelected());
     }
 
-
-
-//    @Override
-//    public void runOpMode() {
-//
-//        AprilTagDetection TagOfInterest = null;
-//
-//        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-//        FtcDashboard dashboard = FtcDashboard.getInstance();
-//        AprilTagProcessor aprilTag = new AprilTagProcessor.Builder()
-//                .setLensIntrinsics(775.79f, 775.79f,400.898f, 300.79f)
-//                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-//                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-//                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-//                .setNumThreads(3)
-//                .setDrawAxes(true)
-//                .setDrawTagID(true)
-//                .setDrawTagOutline(true)
-//                .build();
-//
-//        ColorProcessor itemFinder = new ColorProcessorImpl.Builder()
-//                .setLensIntrinsics(775.79f, 775.79f,400.898f, 300.79f)
-//                .build();
-//
-//        VisionPortal myVisionPortal = new VisionPortal.Builder()
-//                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-//                .setCameraResolution(new Size(800, 600))
-//                .enableLiveView(true)
-//                .addProcessors(aprilTag, itemFinder)
-//                .build();
-//
-//        TelemetryPacket packet2 = new TelemetryPacket();
-//
-//        ArrayList<AprilTagDetection> detections = null;
-//
-//
-//
-//        waitForStart();
-//
-//        while (opModeIsActive()) {
-//
-//            detections = aprilTag.getDetections();
-//
-//            for(AprilTagDetection detection : detections){
-//                if(detection.metadata != null){
-//                    TagOfInterest = detection;
-//                    break;
-//                }
-//            }
-//
-//
-//
-//
-//
-//
-////            if (TagOfInterest != null) {
-////                drive.setDrivePowers(
-////                        new PoseVelocity2d(
-////                                new Vector2d(
-////                                        (-TagOfInterest.ftcPose.y * .2),
-////                                        (-TagOfInterest.ftcPose.x* .2))
-////                                , TagOfInterest.ftcPose.yaw
-////                        ));
-//
-//
-//                drive.updatePoseEstimate();
-//
-//                packet2.fieldOverlay()
-//                        .drawImage("/dash/ftc.jpg", (drive.pose.position.x), (drive.pose.position.y), 13, 13.25, drive.pose.heading.log(), 6.5, 6.625, false);
-//                dashboard.sendTelemetryPacket(packet2);
-//
-//                if (TagOfInterest.metadata != null) {
-//                    telemetry.addData("TagOfInterest ID", TagOfInterest.id);
-//                    telemetry.addData("TagOfInterest X offset", TagOfInterest.ftcPose.x);
-//                    telemetry.addData("TagOfInterest Y offset", TagOfInterest.ftcPose.y);
-//                    telemetry.addData("TagOfInterest yaw", TagOfInterest.ftcPose.yaw);
-//                    telemetry.addData("x", drive.pose.position.x);
-//                    telemetry.addData("y", drive.pose.position.y);
-//                    telemetry.addData("heading", drive.pose.heading);
-//                    telemetry.update();
-//
-//                }
-//            }
-//
-//
-//
-//
-//        }
-//    }
 }
 
